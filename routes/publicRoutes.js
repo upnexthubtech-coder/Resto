f934996f421c3dfbab71b4baac970c4cb59bdfee
UpNextHub <upnexthubtech@gmail.com>
2026-09-15 13:31:09 +0530

    Accept table number in public menu route

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -3,7 +3,7 @@ const { listPublicMenu } = require('../controllers/menuController');
 const { createOrder, getOrderByToken } = require('../controllers/orderController');
 const { createReview } = require('../controllers/reportController');
 
-router.get('/menu/:slug', listPublicMenu);
+router.get('/menu/:slug/:tableNumber?', listPublicMenu);
 router.post('/orders', createOrder);
 router.get('/orders/:token', getOrderByToken);
 router.post('/reviews', createReview);
